3be41349b090646f2c73a0630065d0a595f242a7
Androyen <androyen@aol.com>
2014-10-14 14:29:43 -0500

    Add Sign Up Activity screen

--- gen/com/androyen/ribbit/R.java
+++ gen/com/androyen/ribbit/R.java
@@ -25,31 +25,37 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080008;
+        public static final int emailField=0x7f080005;
         public static final int loginButton=0x7f080002;
         public static final int pager=0x7f080004;
         public static final int passwordField=0x7f080001;
-        public static final int section_label=0x7f080005;
+        public static final int section_label=0x7f080007;
         public static final int signUpText=0x7f080003;
+        public static final int signupButton=0x7f080006;
         public static final int usernameField=0x7f080000;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int fragment_main=0x7f030002;
+        public static final int activity_sign_up=0x7f030002;
+        public static final int fragment_main=0x7f030003;
     }
     public static final class menu {
         public static final int login=0x7f070000;
         public static final int main=0x7f070001;
+        public static final int sign_up=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050005;
         public static final int app_name=0x7f050000;
+        public static final int email_hint=0x7f05000c;
         public static final int hello_world=0x7f050004;
         public static final int login_button_label=0x7f050009;
         public static final int password_hint=0x7f050008;
         public static final int sign_up_label=0x7f05000a;
         public static final int title_activity_login=0x7f050006;
+        public static final int title_activity_sign_up=0x7f05000b;
         public static final int title_section1=0x7f050001;
         public static final int title_section2=0x7f050002;
         public static final int title_section3=0x7f050003;
